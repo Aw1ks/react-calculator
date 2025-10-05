567c21b0fb62a5155baeef096703e39aa35b66a9
Aw1ks <aw1ksss331@gmail.com>
2025-10-05 19:00:12 +0300

    Creating a Display Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 
+import Display from './components/Display';
+
 function App() {
   return (
-    <div className="App">
+    <div className="calculator">
+      <Display/>
     </div>
   );
 }
---
+++ src/components/Display.jsx
@@ -0,0 +1,14 @@
+function Display() {
+    return (
+        <div className="display">
+            <div className="expression">
+                0
+            </div>
+            <div className="result">
+                0
+            </div>
+        </div>
+    )
+}
+
+export default Display;
